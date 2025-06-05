522372461b2957cc0956d36e075954c36c3e3fc0
prog6bt3cecyte losviejones <prog6bt3cecyte@gmail.com>
2025-06-05 21:11:40 +0000

    nuevo cambio

--- dist/routes.js
+++ dist/routes.js
@@ -2,9 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Routes = void 0;
 const usuario_routes_1 = require("./modules/usuario/routes/usuario.routes");
+const comanda_routes_1 = require("./modules/comanda/routes/comanda.routes");
 class Routes {
     constructor() {
-        this.comandaRoutes = new comandaRoutes();
+        this.comandaRoutes = new comanda_routes_1.ComandaRoutes();
         this.usuarioRoutes = new usuario_routes_1.UsuarioRoutes();
     }
     routes(app) {
